772f0da9f79728fdddb700616a47a1bc9184974b
bmaor2 <116643607+bmaor2@users.noreply.github.com>
2023-01-04 22:13:49 +0200

    barDay2

--- Albums.js
+++ Albums.js
@@ -2,14 +2,13 @@ import React, { useContext, useEffect, useState, } from 'react';
 import { Link, NavLink, Outlet } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
 
-
-
-
 export const Albums = () => {
     const { currentUser } = useContext(UserContext);
     const [albumsTitle, setAlbumsTitle] = useState([])
     const [photos, setPhotos] = useState([])
     const [photosArray, setPhotosArray] = useState([])
+    const [drawPhotosArray, setDrawPhotosArray] = useState([])
+    const [photosExists, setPhotosExists] = useState(false)
     const [photoNumber, setPhotoNumber] = useState(1)
 
 
@@ -27,44 +26,44 @@ export const Albums = () => {
 
     }
     const getPhotosOfSpecifiAlbums = async (id) => {
-        // let photosData = await fetch(`https://jsonplaceholder.typicode.com/photos/?albumId=${id}`)
-        // for (let i = 1; i <= 10; i++) {
-            let photosData = await fetch(`https://jsonplaceholder.typicode.com/photos/?albumId=${id}&&id=${(id*50)-50+1}`)
+        for (let i = photoNumber; i < photoNumber + 10; i++) {
+            let photosData = await fetch(`https://jsonplaceholder.typicode.com/photos/?albumId=${id}&&id=${(id * 50) - 50 + i}`)
             let photos = await photosData.json();
-            photosArray.push(photos)
-        // }
-        await setPhotos(photosArray)
+            setPhotosArray([])
+            photosArray.push(photos[0])
+        }
+        setPhotos(photosArray)
+        console.log(photosArray);
         return photos;
     }
     const showPhotosOfAlbum = async (id) => {
+        let tempArray = [];
         let photosById = await getPhotosOfSpecifiAlbums(id)
-
-        // let tempArr = [];
-        // for (let i = 0; i < 10 ; i++) {
-        //     tempArr.push(photosById[i]);
-        // }
-        let photosArray = photosById.map((photo, index) => <img src={photo.thumbnailUrl} key={index} />)
-        setPhotos(photosArray);
+        tempArray = photosById.map((photo, index) => <img key={index} src={photo.thumbnailUrl} />)
+        setPhotosExists(true);
+        await setDrawPhotosArray(tempArray)
 
     }
 
     const next10PhotosBtn = async (event) => {
-
+        setPhotoNumber(photoNumber + 10);
+        console.log(photoNumber);
     }
     useEffect(() => {
         getAllAlbums()
     }, [])
-    let albumsTitleMap = albumsTitle.map((title, index) =>
-        <>
-
-            <p key={index} onClick={() => showPhotosOfAlbum(index + 1)} > {title} </p>
-
-        </>
+    useEffect(() => {
+        
+    }, [photoNumber])
+    let albumsTitleMap = albumsTitle.map((title, index) => <p key={index} onClick={() => showPhotosOfAlbum(index + 1)} > {title} </p>
     )
     return (
         <div className='albumsContainer'>
             {albumsTitleMap}
-            {photos}
+            <div>
+                {drawPhotosArray}
+                {photosExists ? <button onClick={next10PhotosBtn}>more photos</button>: null}
+            </div>
         </div>
     )
 }
